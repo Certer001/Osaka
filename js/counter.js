45f28aaac7b00b1dd718b01aef3eae744597282c
Certer001 <76663981+Certer001@users.noreply.github.com>
2023-07-28 16:00:01 +0300

    counter 1

---
+++ js/counter.js
@@ -0,0 +1,10 @@
+const btnMinus = document.querySelector('[data-action="minus"]');
+const btnPlus = document.querySelector('[data-action="plus"]');
+
+btnMinus.addEventListener('click', function() {
+    console.log('minus');
+});
+
+btnPlus.addEventListener('click', function() {
+    console.log('plus');
+})
